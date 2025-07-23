963652e86327d8c6bb6017e7945660f713cecb31
Dimitar <dimitar.georgiev@primeholding.com>
2025-07-24 01:51:51 +0300

    Include ServerHttpsPort in esp32 cam secrets

--- ESP_32_Cam_Firmware/include/secrets.h
+++ ESP_32_Cam_Firmware/include/secrets.h
@@ -1,6 +1,7 @@
 #define WIFI_SSID "your_wifi_ssid_here"
 #define WIFI_PASS "your_wifi_password_here"
 #define SharedSecretCamKey "your_shared_secret_key_here(should be the same as in the server code)"
+#define ServerHttpsPort 7010
 #define ROOT_CA_CERT "-----BEGIN CERTIFICATE-----\n\
 YOUR_ROOT_CA_CERTIFICATE_HERE\n\
 -----END CERTIFICATE-----\n"
